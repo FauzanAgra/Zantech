eb0627c392d4324552a0f52b33280059daeda0c0
FauzanAgra <fauzanagra1@gmail.com>
2020-07-07 03:29:57 +0700

    Menambahkan Landing page

---
+++ zantech.sql
@@ -0,0 +1,251 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 06, 2020 at 10:28 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `zantech`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `list_movies`
+--
+
+CREATE TABLE `list_movies` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `year` int(10) NOT NULL,
+  `rating` varchar(10) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `date_updated` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `list_movies`
+--
+
+INSERT INTO `list_movies` (`id`, `name`, `year`, `rating`, `image`, `date_updated`) VALUES
+(1, 'The Hobbit: An Unexpected Journey', 2012, '7.8', 'film_the_hobbit_2012.jpg', 1593955017),
+(2, 'The Hobbit: The Desolation of Smaug', 2013, '7.8', 'film_the_hobbit_2013.jpg', 1593955997),
+(3, 'The Hobbit: The Battle of the Five Armies', 2014, '7.4', 'film_the_hobbit_2014.jpg', 1593956151),
+(6, 'The Lord of the Rings: The Fellowship of the Ring', 2001, '8.8', 'film_the_lord_2001.jpg', 1593975528),
+(7, 'The Lord of the Rings: The Two Towers', 2002, '8.7', 'film_the_lord_2002.jpg', 1594008653),
+(8, 'The Lord of the Rings: The Return of the King', 2003, '8.9', 'film_the_lord_2003.jpg', 1594009697);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `image` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `is_active` int(1) NOT NULL,
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
+(3, 'Fauzan Agra', 'fauzanagra1@gmail.com', 'saya.jpg', '$2y$10$UVQHjLxTGvf5lubbbl0wvuBy2AFzxCe9HAMqRDdPvAvt4mlIUUh5W', 1, 1, 1593064808),
+(4, 'Rizky Dwi', 'rizkydwi@gmail.com', 'default.jpg', '$2y$10$Yeo/pfRuipfnlVHG0n01D.f7yJzI52ofcTr2nPgXmTjdL.J8EnLAG', 2, 1, 1593088345);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_acces_menu`
+--
+
+CREATE TABLE `user_acces_menu` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_acces_menu`
+--
+
+INSERT INTO `user_acces_menu` (`id`, `role_id`, `menu_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 2),
+(4, 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_menu`
+--
+
+CREATE TABLE `user_menu` (
+  `id` int(11) NOT NULL,
+  `menu` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_menu`
+--
+
+INSERT INTO `user_menu` (`id`, `menu`) VALUES
+(1, 'Admin'),
+(2, 'User'),
+(3, 'Menu'),
+(9, 'TestTos');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `role`) VALUES
+(1, 'Administrator'),
+(2, 'Member');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_sub_menu`
+--
+
+CREATE TABLE `user_sub_menu` (
+  `id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `url` varchar(128) NOT NULL,
+  `icon` varchar(128) NOT NULL,
+  `is_active` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_sub_menu`
+--
+
+INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`, `icon`, `is_active`) VALUES
+(1, 1, 'Dashboard', 'admin', 'fas fa-fw fa-tachometer-alt', 1),
+(2, 2, 'My Profile', 'user', 'fas fa-fw fa-user-circle', 1),
+(3, 2, 'Edit Profile', 'user/edit', 'fas fa-fw fa-user-edit', 1),
+(4, 3, 'Menu Management', 'menu', 'fas fa-fw fa-folder-plus', 1),
+(5, 3, 'Submenu Management', 'menu/submenu', 'far fa-fw fa-folder', 1),
+(7, 1, 'Role', 'admin/role', 'fas fa-fw fa-user-tie', 1),
+(8, 2, 'Change Password', 'user/changepassword', 'fas fa-fw fa-key', 1),
+(10, 2, 'List Movies', 'user/listmovies', 'fas fa-fw fa-cubes', 1),
+(11, 1, 'Movies', 'admin/movies', 'fas fa-fw fa-film', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `list_movies`
+--
+ALTER TABLE `list_movies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_acces_menu`
+--
+ALTER TABLE `user_acces_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `list_movies`
+--
+ALTER TABLE `list_movies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user_acces_menu`
+--
+ALTER TABLE `user_acces_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
